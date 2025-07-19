e63c3fe600cff6cf85e49bba18d23cd740f9a16f
leonardo-lima-98 <leonardodelima98@outlook.com>
2025-07-19 15:50:51 -0300

    Versão : Atualização automatizada

--- src/app.py
+++ src/app.py
@@ -6,3 +6,15 @@ app = FastAPI()
 @app.get('/')
 def read_root():
     return {'message': 'Hello, World!'}
+
+@app.get('/inexistente')
+def read_inexistente():
+    return {'detail': 'Not Found'}, 404
+
+@app.post('/')
+def post_root():
+    return {'detail': 'Method Not Allowed'}, 405
+
+@app.exception_handler(404)
+def not_found_exception_handler(request, exc):
+    return {'detail': 'Not Found'}, 404
